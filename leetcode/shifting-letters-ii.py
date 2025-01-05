ddec66305732754ea2dab8afc9f64df79c8e0648
Ratul Roy <19484371+phase7@users.noreply.github.com>
2025-01-05 12:08:23 +0000

    refactor shiftingLetters method for clarity and efficiency

--- leetcode/shifting-letters-ii.py
+++ leetcode/shifting-letters-ii.py
@@ -1,22 +1,19 @@
-
 from functools import cache
-import numpy as np
-
+import numpy
 class Solution:
     def shiftingLetters(self, s: str, shifts: list[list[int]]) -> str:
-        s = list(s)
-        rep = np.zeros(len(s), dtype=int)
+        rep = numpy.zeros(len(s)+1, dtype=int)
 
         @cache
-        def shifted_char(c: str, shift: int = 1) -> str:
-            return chr((ord(c) - ord('a') + shift) % 26 + ord('a'))
+        def shifted_char(c: str, shift: int) -> str:
+            return chr((ord(c) - 97 + shift) % 26 + 97)
 
 
         for start, end, direction in shifts:
             shift = 1 if direction else -1
             rep[start:end+1] += shift
 
-        return "".join([shifted_char(c, shift) for c, shift in zip(s, rep)])
+        return "".join(shifted_char(c, shift) for c, shift in zip(s, rep))
 
 if __name__ == "__main__":
     assert Solution().shiftingLetters(s = "abc", shifts = [[0,1,0],[1,2,1],[0,2,1]]) == "ace", Solution().shiftingLetters(s = "abc", shifts = [[0,1,0],[1,2,1],[0,2,1]])
